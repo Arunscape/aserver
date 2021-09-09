32215ce7e0a0696a0da235b38e26cb4488ca74f4
Arunscape <8227297+Arunscape@users.noreply.github.com>
2021-09-09 01:06:29 -0600

    async

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,35 @@
 #![warn(clippy::all)]
 #[allow(dead_code)]
-use std::net::{TcpListener, TcpStream};
-use std::thread;
-use std::io::prelude::*;
 use std::str::FromStr;
+use async_std::prelude::*;
+use async_std::net::{TcpStream, TcpListener};
+use futures::stream::StreamExt;
 
 mod http;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("0.0.0.0:8080")?;
+//mod threadpool;
+#[async_std::main]
+async fn main() {
+    let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
     let port = "8080";
     println!("Listening on {}", port);
 
-    for stream in listener.incoming() {
-        let stream = stream?;
-        thread::spawn(move || {
-            handle_client(stream);
-        });
-    }
-    Ok(())
+    listener.incoming().for_each_concurrent(None, |stream| async move  {
+        let stream = stream.unwrap();
+        async_std::task::spawn(handle_client(stream));
+    }).await;
 }
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream) {
 
     let mut buffer  = Vec::new();
 
-    stream.read_to_end(&mut buffer).expect("Failed to read stream into buffer");
+    stream.read_to_end(&mut buffer).await.expect("Failed to read stream into buffer");
 
     let response = "HTTP/1.1 200 OK\r\n\r\n";
 
-    stream.write(response.as_bytes()).expect("Failed to send response");
+    stream.write(response.as_bytes()).await.expect("Failed to send response");
 
-    stream.flush().expect("Failed to flush stream response");
+    stream.flush().await.expect("Failed to flush stream response");
 
     let x = String::from_utf8_lossy(&buffer);
     dbg!(&x);
